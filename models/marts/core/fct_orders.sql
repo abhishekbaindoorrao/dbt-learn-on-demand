ed9f9d5ec8ea3fa8278dea3fb1fd718ed08dff35
arao@dunelm.com <arao@dunelm.com>
2021-08-14 22:37:32 +0000

    solution for task 1

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,10 +1,26 @@
 with payments as (
-    select * from raw.stripe.payment limit 10
+    select *
+    from {{ ref('stg_payment')}}
 ),
 orders as (
-    select * from {{ ref('stg_orders')}}
+    select * 
+    from {{ ref('stg_orders')}}
+),
+
+successful_payments as (
+    select * from payments 
+    where status='success'
 )
 
-select order_id, customer_id, amount from orders
+
+select 
+    order_id, 
+    customer_id, 
+    coalesce(amount, 0) as amount,
+    orders.status,
+    order_date
+
+from orders
 inner join
-payments on orders.order_id=payments.orderid
+payments on 
+orders.order_id=payments.orderid
